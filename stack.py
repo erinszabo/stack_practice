b99132f89d61635f7bc2c88ad9bda61265bafaec
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-10-27 16:24:54 -0700

    Initial commit

---
+++ stack.py
@@ -0,0 +1,46 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+
+def parChecker(symbolString):
+    """
+    checks if parentheses are balanced
+    :param symbolString:
+    :return: Boolean
+    """
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
