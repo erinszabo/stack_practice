7b28206a2b65d2aac921f607b40020ce6822dbdc
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-10-27 20:31:31 -0700

    helpful comments

--- stack.py
+++ stack.py
@@ -5,13 +5,13 @@ class Stack:  # a stack of blocks
     def is_empty(self):
         return self.items == []
 
-    def push(self, item):  # place a new block on the stack
+    def push(self, item):  # place a new block on the stack O(1)
         self.items.append(item)
 
-    def pop(self):  # look at and remove the top block
+    def pop(self):  # look at and remove the top block O(1), bottom block O(n)
         return self.items.pop()
 
-    def peek(self):  # just look at the top block
+    def peek(self):  # just look at the top block O(1), bottom block O(n)
         return self.items[len(self.items) - 1]
 
     def size(self):
