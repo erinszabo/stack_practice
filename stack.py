58ffdf50722ae030b593c40b7056d52fc694c156
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-10-27 18:09:26 -0700

    ,

--- stack.py
+++ stack.py
@@ -84,12 +84,7 @@ def base_converter(decNumber, base):
 
 
 def infixToPostfix(infixexpr):
-    prec = {}
-    prec["*"] = 3
-    prec["/"] = 3
-    prec["+"] = 2
-    prec["-"] = 2
-    prec["("] = 1
+    prec = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
     opStack = Stack()
     postfixList = []
     tokenList = infixexpr.split()
@@ -113,3 +108,29 @@ def infixToPostfix(infixexpr):
     while not opStack.is_empty():
         postfixList.append(opStack.pop())
     return " ".join(postfixList)
+
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+            operandStack.push(result)
+    return operandStack.pop()
+
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
