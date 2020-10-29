79c30715944280191c9298eefbc5f7f46e9da7ce
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-10-29 11:47:01 -0700

    reformatted

--- recursion.py
+++ recursion.py
@@ -3,15 +3,16 @@ def binarySearch(alist, item):
         return False
     else:
         midpoint = len(alist)//2
-        if alist[midpoint]==item:
-          return True
+        if alist[midpoint] == item:
+            return True
         else:
-          if item<alist[midpoint]:
-            return binarySearch(alist[:midpoint],item)
-          else:
-            return binarySearch(alist[midpoint+1:],item)
+            if item < alist[midpoint]:
+                return binarySearch(alist[:midpoint], item)
+            else:
+                return binarySearch(alist[midpoint+1:], item)
 
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42, ]
 
 
 print(binarySearch(testlist, 3))
